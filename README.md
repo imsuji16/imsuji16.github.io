78199177a493687b5e5fdedc2fe8bed15095d4df
ImSuji <sujit.dahal022@gmail.com>
2025-11-02 16:11:16 +0000

    Created basic framework for website header, added banner parallax effect

--- README.md
+++
@@ -1,1 +0,0 @@
-# imsuji16.github.io
---
+++ page.js
@@ -0,0 +1,9 @@
+const banner = document.getElementById("banner");
+const bannerImage = document.getElementById("banner-image");
+
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 10;
+    const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+    bannerImage.style.transform = `translate(${x}px, ${y}px) scale(1.1)`;
+});
